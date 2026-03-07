312f8300226f0edd8cff6dae8aec60d6fdb3bb1f
chrisotero2026 <chrisotero2012@gmail.com>
2026-03-07 17:39:24 -0500

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Providers } from '@/app/providers';
+import { Providers } from './providers';
 
 export const dynamic = 'force-dynamic';
 
